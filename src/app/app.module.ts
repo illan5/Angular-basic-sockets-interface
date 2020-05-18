aec53cee17be5ce84e0c482d3140569c8fc58743
Jose Illan <jose_illan@hotmail.com>
2020-05-18 22:13:40 +0200

    Basic sockets configuration done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Sockets
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+const config: SocketIoConfig = { 
+  url: environment.wsUrl, options: {} 
+};
+
+
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment.prod';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
