99603b7da7f3c579fe720d44667524fa5f307742
Jose Illan <jose_illan@hotmail.com>
2020-05-20 23:36:10 +0200

    Create private messages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, CanActivate } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { MensajesComponent } from './pages/mensajes/mensajes.component';
+import { UsuarioGuardService } from './guards/usuario-guard.service';
 
 const appRoutes = [
   { path: '', component: LoginComponent },
-  { path: 'mensajes', component: MensajesComponent },
+  { 
+    path: 'mensajes', 
+    component: MensajesComponent,
+    canActivate: [ UsuarioGuardService ]
+   },
   { path: '**', component: LoginComponent }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { WebsocketService } from './services/websocket.service';
-//import { ChatService } from './services/chat.service';
+import { ChatService } from './services/chat.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-//export class AppComponent implements OnInit {
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'basico';
 
   constructor( 
     public wsService: WebsocketService,
-    //public chatService: ChatService
+    public chatService: ChatService
      ){
 
   }
 
-  // ngOnInit(){
-  //   this.chatService.sendMessage('Hello from Angular');
-  // }
+  ngOnInit() {
+    this.chatService.getMessagesPrivate().subscribe( msg => {
+      console.log(msg);
+    } )
+  }
 }
